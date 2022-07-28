975c6ef6238c1ab206db2f20c0d1ef3015cf831b
khaled hussien <57367756+khaled2024@users.noreply.github.com>
2022-07-28 12:10:16 +0200

    Initial Commit

---
+++ SwiftUiApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUiApp
+//
+//  Created by KhaleD HuSsien on 28/07/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUiApp/SwiftUiAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUiAppApp.swift
+//  SwiftUiApp
+//
+//  Created by KhaleD HuSsien on 28/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUiAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
